5579d59e11cce43682a67197f10a478d20e467b6
r0mille <ke4mtg+r0mille@gmail.com>
2020-03-22 23:38:38 -0400

    feat/airtable-and-hooks -- fix indent

--- food-supplier-mkt-app/clientside-reactjs/src/Mapbox.js
+++ food-supplier-mkt-app/clientside-reactjs/src/Mapbox.js
@@ -8,39 +8,36 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_TOKEN
 function airtableFetchMarkers(map) {
 	const base = new Airtable({apiKey: 'AIRTBABLE_API_KEY'}).base('appDk0v3oHfD3Bjf6');
 
-	base('Locations').select({
-		view: "Grid view"
-	}).eachPage(function page(records, fetchNextPage) {
-			// This function (`page`) will get called for each page of records.
-			records.forEach(function(record) {
+  base('Locations').select({
+    view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+    records.forEach(function(record) {
+      if (record.fields.Longitude && record.fields.Latitude) {
+        const coordinates = [record.fields.Longitude, record.fields.Latitude];
 
-				if (record.fields.Longitude && record.fields.Latitude) {
-					const coordinates = [record.fields.Longitude, record.fields.Latitude];
+        // make a marker and add to the map
+        new mapboxgl.Marker()
+        .setLngLat(coordinates)
+        .addTo(map);
 
-					// make a marker and add to the map
-					new mapboxgl.Marker()
-						.setLngLat(coordinates)
-						.addTo(map);
+        console.log('Retrieved', record.get('Name'));
+      }
+    });
 
-					console.log('Retrieved', record.get('Name'));
-				}
-
-			});
-
-			// To fetch the next page of records, call `fetchNextPage`.
-			// If there are more records, `page` will get called again.
-			// If there are no more records, `done` will get called.
-			fetchNextPage();
-
-	}, function done(err) {
-			if (err) { console.error(err); return; }
-	});
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+  }, function done(err) {
+    if (err) { console.error(err); return; }
+  });
 }
 
 export default function Mapbox(props) {
   let map = null;
   const [view, setView] = useState({
-		// Default Bay Area coordinates
+    // Default Bay Area coordinates
     lng: -122.420679,
     lat: 37.772537,
     zoom: 9,
