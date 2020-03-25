fe6a18db589a2b069fe5b15e39770d7e55f165ef
Paul <paulglujan@gmail.com>
2020-03-25 10:52:38 -0700

    Added a generic card inset element

--- food-supplier-mkt-app/clientside-reactjs/src/App.js
+++ food-supplier-mkt-app/clientside-reactjs/src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Card, Layout } from 'antd';
+import { createUseStyles } from 'react-jss';
 import Mapbox from './Mapbox.js';
 
 const { Content, Header } = Layout;
 
-export default function App(props) {
+const App = props => {
+  const classes = useStyles();
   return (
     <Layout>
       <Header>Save Our Farms</Header>
       <Content>
         <Mapbox />
+        <div className={classes.cardPadding}>
+          <Card
+            title="Default size card"
+            style={{ width: 300 }}
+            className={classes.mapCard}
+          >
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </div>
       </Content>
     </Layout>
   );
-}
+};
+
+const useStyles = createUseStyles({
+  cardPadding: {
+    padding: 20
+  },
+  mapCard: {
+    position: 'absolute'
+  }
+});
+
+export default App;
