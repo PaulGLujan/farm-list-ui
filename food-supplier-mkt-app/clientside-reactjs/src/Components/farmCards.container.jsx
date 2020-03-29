83d58650da31e1f8a45f5b2944c578c10e51f13c
lmjcbs <hello@liamjacobs.dev>
2020-03-29 20:09:32 +0100

    add initial farm fata

--- food-supplier-mkt-app/clientside-reactjs/src/Components/farmCards.container.jsx
+++ food-supplier-mkt-app/clientside-reactjs/src/Components/farmCards.container.jsx
@@ -10,12 +10,60 @@ const farms = [
     imageURL:
       'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/sea_forager.jpg',
     tags: ['CSA', 'Local Pickup']
+  },
+  {
+    name: 'Water2Table',
+    type: 'Fish',
+    websiteURL: 'https://www.water2table.com/',
+    imageURL:
+      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/water_2_table.png',
+    tags: ['Local Pickup', 'Local Delivery']
+  },
+  {
+    name: 'TwoXSea',
+    type: 'Fish',
+    websiteURL: 'http://twoxsea.com/',
+    imageURL:
+      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/two_x_sea.jpg',
+    tags: ['Local Pickup']
+  },
+  {
+    name: 'Dirty Girl Produce',
+    type: 'Farm',
+    websiteURL: 'http://www.dirtygirlproduce.com/',
+    imageURL:
+      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/dirty_girl_produce.gif',
+    tags: ['Organic', 'CSA']
+  },
+  {
+    name: 'Liberty Ducks',
+    type: 'Poultry',
+    websiteURL: 'http://www.libertyducks.com/',
+    imageURL:
+      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/liberty_ducks.jpeg',
+    tags: ['Local Pickup', 'Local Delivery']
+  },
+  {
+    name: 'Riverdog Farm',
+    type: 'Farm',
+    websiteURL: 'http://www.riverdogfarm.com/',
+    imageURL:
+      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/riverdog_farm.jpg',
+    tags: ['Organic', 'CSA']
+  },
+  {
+    name: 'CreamCo Meats',
+    type: 'Meat',
+    websiteURL: 'http://www.dirtygirlproduce.com/',
+    imageURL:
+      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/creamco_meats.png',
+    tags: ['Local Pickup', 'Local Delivery']
   }
 ];
 
 const FarmCards = () => {
   return (
-    <Card title="Default size card" style={{ width: 300 }}>
+    <Card title="Default size card" style={{ width: 310 }}>
       {farms.map(farmData => (
         <Farm data={farmData}></Farm>
       ))}
