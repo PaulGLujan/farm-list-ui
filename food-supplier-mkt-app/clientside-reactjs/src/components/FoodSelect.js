67f63c449f5d9f8f22a3ff642c7bad709a0a0be9
Paul <paulglujan@gmail.com>
2020-03-29 12:12:26 -0700

    Moved request into utilites folder

--- food-supplier-mkt-app/clientside-reactjs/src/components/FoodSelect.js
+++ food-supplier-mkt-app/clientside-reactjs/src/components/FoodSelect.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Select } from 'antd';
+import { getAirtableData } from '../utilities/requestOperations';
 import axios from 'axios';
 
 const { Option } = Select;
@@ -14,33 +15,8 @@ class FoodSelect extends Component {
     }
 
     async componentDidMount() {
-        // Example using axios to http get
-        try {
-            var headers = {
-                Authorization: 'Bearer AIRTBABLE_API_KEY',
-                'Content-Type': 'application/json'
-            };
-            var response_foodsTable = await axios(
-                'https://api.airtable.com/v0/appDk0v3oHfD3Bjf6/Food%20Items?maxRecords=3&view=Grid%20view&maxRecords=100',
-                { headers }
-            );
-            console.log('response_foodsTable.data', response_foodsTable.data);
-            var dataReceived = response_foodsTable.data.records;
-
-            var listOfFoodsToSet = [];
-            dataReceived.map(item => {
-                if (item['fields']['Name']) {
-                    listOfFoodsToSet.push(item['fields']['Name']);
-                }
-            });
-
-            console.log('listOfFoodsToSet -- ', listOfFoodsToSet);
-            this.setState({
-                listOfFoods: listOfFoodsToSet
-            });
-        } catch (err) {
-            alert(err);
-        }
+        const listOfFoods = await getAirtableData();
+        this.setState({ listOfFoods });
     }
 
     render() {
