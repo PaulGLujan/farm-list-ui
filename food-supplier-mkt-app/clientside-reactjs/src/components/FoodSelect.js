1afe3ce731373d29715e7074ccc853b15135cb5a
lmjcbs <hello@liamjacobs.dev>
2020-03-30 11:36:52 +0100

    refactored to functional component with hooks

--- food-supplier-mkt-app/clientside-reactjs/src/components/FoodSelect.js
+++ food-supplier-mkt-app/clientside-reactjs/src/components/FoodSelect.js
@@ -1,49 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Select } from 'antd';
 import { getAirtableData } from '../utilities/requestOperations';
 
-const { Option } = Select;
+const FoodSelect = ({ onChange }) => {
+    const [listOfFoods, setListOfFoods] = useState([])
+    const isLoading = listOfFoods.length ? false : true;
+    const { Option } = Select;
 
-class FoodSelect extends Component {
-    constructor(props) {
-        super(props)
-        this.handleChange = this.handleChange.bind(this)
-        this.state = {
-            listOfFoods: []
-        };
-    }
+    useEffect(() => {
+        async function fetchFoodsList(){
+            try {
+                const result = await getAirtableData();
+                setListOfFoods(result)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchFoodsList()
+    }, [])
 
-    handleChange(value) {
-        this.props.onChange(value)
-    }
-
-    async componentDidMount() {
-        const listOfFoods = await getAirtableData();
-        this.setState({ listOfFoods });
-    }
-
-    render() {
-      const isLoading = this.state.listOfFoods.length ? false : true;
-
-        return (
-            <>
-                <Select
-                    mode="multiple"
-                    size={'default'}
-                    placeholder="Please select food"
-                    onChange={this.handleChange}
-                    style={{ width: '100%', marginBottom: 12 }}
-                    loading={isLoading}
-                >
-                    {this.state.listOfFoods.map((food, i) => (
-                        <Option key={i} value={food}>
-                            {food}
-                        </Option>
-                    ))}
-                </Select>
-            </>
-        );
-    }
+    return (
+        <Select
+            mode="multiple"
+            size={'default'}
+            placeholder="Please select food"
+            onChange={onChange}
+            style={{ width: '100%', marginBottom: 12 }}
+            loading={isLoading}
+        >
+            {listOfFoods.map((food, i) => (
+                <Option key={i} value={food}>
+                    {food}
+                </Option>
+            ))}
+        </Select>
+    )
 }
 
 export default FoodSelect;
