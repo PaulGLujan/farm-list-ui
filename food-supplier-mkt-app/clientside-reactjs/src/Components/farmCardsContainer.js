bfd5ec74afcf55b7998fa41202cab86a724d0125
Paul <paulglujan@gmail.com>
2020-03-29 21:13:10 -0700

    Added text around search bar

--- food-supplier-mkt-app/clientside-reactjs/src/Components/farmCardsContainer.js
+++ food-supplier-mkt-app/clientside-reactjs/src/Components/farmCardsContainer.js
@@ -5,84 +5,89 @@ import Farm from './farmCard';
 import FoodSelect from './FoodSelect';
 
 const farms = [
-  {
-    name: 'Seaforager',
-    type: 'Fish',
-    websiteURL: 'https://www.seaforager.com/',
-    imageURL:
-      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/sea_forager.jpg',
-    tags: ['CSA', 'Pickup']
-  },
-  {
-    name: 'Water2Table',
-    type: 'Fish',
-    websiteURL: 'https://www.water2table.com/',
-    imageURL:
-      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/water_2_table.png',
-    tags: ['Pickup', 'Local Delivery']
-  },
-  {
-    name: 'TwoXSea',
-    type: 'Fish',
-    websiteURL: 'http://twoxsea.com/',
-    imageURL:
-      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/two_x_sea.jpg',
-    tags: ['Pickup']
-  },
-  {
-    name: 'Dirty Girl Produce',
-    type: 'Farm',
-    websiteURL: 'http://www.dirtygirlproduce.com/',
-    imageURL:
-      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/dirty_girl_produce.gif',
-    tags: ['Organic', 'CSA']
-  },
-  {
-    name: 'Liberty Ducks',
-    type: 'Poultry',
-    websiteURL: 'http://www.libertyducks.com/',
-    imageURL:
-      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/liberty_ducks.jpeg',
-    tags: ['Pickup', 'Local Delivery']
-  },
-  {
-    name: 'Riverdog Farm',
-    type: 'Farm',
-    websiteURL: 'http://www.riverdogfarm.com/',
-    imageURL:
-      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/riverdog_farm.jpg',
-    tags: ['Organic', 'CSA']
-  },
-  {
-    name: 'CreamCo Meats',
-    type: 'Meat',
-    websiteURL: 'http://www.dirtygirlproduce.com/',
-    imageURL:
-      'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/creamco_meats.png',
-    tags: ['Pickup', 'Local Delivery']
-  }
+    {
+        name: 'Seaforager',
+        type: 'Fish',
+        websiteURL: 'https://www.seaforager.com/',
+        imageURL:
+            'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/sea_forager.jpg',
+        tags: ['CSA', 'Pickup']
+    },
+    {
+        name: 'Water2Table',
+        type: 'Fish',
+        websiteURL: 'https://www.water2table.com/',
+        imageURL:
+            'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/water_2_table.png',
+        tags: ['Pickup', 'Local Delivery']
+    },
+    {
+        name: 'TwoXSea',
+        type: 'Fish',
+        websiteURL: 'http://twoxsea.com/',
+        imageURL:
+            'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/two_x_sea.jpg',
+        tags: ['Pickup']
+    },
+    {
+        name: 'Dirty Girl Produce',
+        type: 'Farm',
+        websiteURL: 'http://www.dirtygirlproduce.com/',
+        imageURL:
+            'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/dirty_girl_produce.gif',
+        tags: ['Organic', 'CSA']
+    },
+    {
+        name: 'Liberty Ducks',
+        type: 'Poultry',
+        websiteURL: 'http://www.libertyducks.com/',
+        imageURL:
+            'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/liberty_ducks.jpeg',
+        tags: ['Pickup', 'Local Delivery']
+    },
+    {
+        name: 'Riverdog Farm',
+        type: 'Farm',
+        websiteURL: 'http://www.riverdogfarm.com/',
+        imageURL:
+            'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/riverdog_farm.jpg',
+        tags: ['Organic', 'CSA']
+    },
+    {
+        name: 'CreamCo Meats',
+        type: 'Meat',
+        websiteURL: 'http://www.dirtygirlproduce.com/',
+        imageURL:
+            'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/creamco_meats.png',
+        tags: ['Pickup', 'Local Delivery']
+    }
 ];
 
 const FarmCards = () => {
-  const classes = useStyles();
-  const { card } = classes;
-  return (
-    <Card title="Default size card" className={card}>
-      <FoodSelect />
-      {farms.map(farmData => (
-        <Farm data={farmData} />
-      ))}
-    </Card>
-  );
+    const classes = useStyles();
+    const { card } = classes;
+    return (
+        <Card title="Which foods are you searching for?" className={card}>
+            <div className={classes.cardTitle}>
+                Please note: We are currently only serving the San Francisco Bay
+                Area. We will expand from there.
+            </div>
+            <br />
+            <FoodSelect />
+            {farms.map(farmData => (
+                <Farm data={farmData} />
+            ))}
+        </Card>
+    );
 };
 
 const useStyles = createUseStyles({
-  card: {
-    width: 400,
-    overflow: 'auto',
-    maxHeight: '85vh',
-    borderRadius: 10
-  }
+    card: {
+        width: 400,
+        overflow: 'auto',
+        maxHeight: '85vh',
+        borderRadius: 10
+    }
 });
 
 export default FarmCards;
