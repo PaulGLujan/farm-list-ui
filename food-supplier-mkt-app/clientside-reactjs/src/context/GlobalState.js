37224f84cb8094fd1bd1748bc238a8783eeb9c3d
lmjcbs <hello@liamjacobs.dev>
2020-03-30 22:29:18 +0100

    refactor into one api request

--- food-supplier-mkt-app/clientside-reactjs/src/context/GlobalState.js
+++ food-supplier-mkt-app/clientside-reactjs/src/context/GlobalState.js
@@ -1,91 +1,37 @@
 import React, { useState } from 'react';
 import FarmContext from './farm-context';
-import { getAirtableData } from '../utilities/requestOperations';
+import { getAllAirtableData } from '../utilities/requestOperations';
 
 const GlobalState = props => {
-  const [farms, setFarms] = useState(
-    [
-      {
-          name: 'Seaforager',
-          type: 'Fish',
-          websiteURL: 'https://www.seaforager.com/',
-          imageURL:
-              'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/sea_forager.jpg',
-          tags: ['CSA', 'Pickup']
-      },
-      {
-          name: 'Water2Table',
-          type: 'Fish',
-          websiteURL: 'https://www.water2table.com/',
-          imageURL:
-              'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/water_2_table.png',
-          tags: ['Pickup', 'Local Delivery']
-      },
-      {
-          name: 'TwoXSea',
-          type: 'Fish',
-          websiteURL: 'http://twoxsea.com/',
-          imageURL:
-              'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/two_x_sea.jpg',
-          tags: ['Pickup']
-      },
-      {
-          name: 'Dirty Girl Produce',
-          type: 'Farm',
-          websiteURL: 'http://www.dirtygirlproduce.com/',
-          imageURL:
-              'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/dirty_girl_produce.gif',
-          tags: ['Organic', 'CSA']
-      },
-      {
-          name: 'Liberty Ducks',
-          type: 'Poultry',
-          websiteURL: 'http://www.libertyducks.com/',
-          imageURL:
-              'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/liberty_ducks.jpeg',
-          tags: ['Pickup', 'Local Delivery']
-      },
-      {
-          name: 'Riverdog Farm',
-          type: 'Farm',
-          websiteURL: 'http://www.riverdogfarm.com/',
-          imageURL:
-              'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/riverdog_farm.jpg',
-          tags: ['Organic', 'CSA']
-      },
-      {
-          name: 'CreamCo Meats',
-          type: 'Meat',
-          websiteURL: 'http://www.dirtygirlproduce.com/',
-          imageURL:
-              'https://helplocalfarms-images.s3-us-west-2.amazonaws.com/creamco_meats.png',
-          tags: ['Pickup', 'Local Delivery']
-      }
-    ]
-  )
-
-  // TODO: Farms still hard coded, write fetchFarms() to request entire database
-  // const fetchFarms = () => {
-  //   setFarms()
-  // }
-
-  // Fetches the food types column from Airtable database
+  const [farms, setFarms] = useState([])
   const [farmFoodTypes, setFarmFoodTypes] = useState([])
-  const fetchFarmFoodTypes = async () => {
-      try {
-          const result = await getAirtableData();
-          setFarmFoodTypes(result)
-      } catch (err) {
-          console.log(err)
-      }
+
+  const fetchAllFarmData = async () => {
+    try {
+        // getAllAirtableData() returns an object containing two arrays
+        // for complete farm data and unique food types.
+        const response = await getAllAirtableData();
+        const { farms, uniqueFarmFoodTypes } = response
+
+        setFarms(farms)
+        setFarmFoodTypes(uniqueFarmFoodTypes)
+    } catch (err) {
+        console.log(err)
+    }
   }
 
+  // Values are global through useContext
+  // Example usage:
+  //
+  // ** import React, { useContext } from 'react' **
+  // ** import FarmContext from './context/farm-context.js **
+  //
+  // const { farms } = useContext(FarmContext)
   return (
     <FarmContext.Provider value={{
       farms,
-      // fetchFarms,
       farmFoodTypes,
-      fetchFarmFoodTypes,
+      fetchAllFarmData,
     }}>
       {props.children}
     </FarmContext.Provider>
