ad4e861732db5cb3fe2bb21d3ad422a6e6800871
lmjcbs <hello@liamjacobs.dev>
2020-03-30 18:09:30 +0100

    get farm food types using useContext

--- food-supplier-mkt-app/clientside-reactjs/src/components/FoodSelect.js
+++ food-supplier-mkt-app/clientside-reactjs/src/components/FoodSelect.js
@@ -1,26 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { createUseStyles } from 'react-jss';
 import { Select } from 'antd';
-import { getAirtableData } from '../utilities/requestOperations';
+import FarmContext from '../context/farm-context';
 
 const FoodSelect = ({ onChange }) => {
-    const [listOfFoods, setListOfFoods] = useState([])
-    const isLoading = listOfFoods.length > 0 ? false : true;
+    const { farmFoodTypes, fetchFarmFoodTypes } = useContext(FarmContext)
+    const isLoading = farmFoodTypes.length > 0 ? false : true;
     const classes = useStyles();
     const { selectBox } = classes
     const { Option } = Select;
 
     useEffect(() => {
-        async function fetchFoodsList(){
-            try {
-                const result = await getAirtableData();
-                setListOfFoods(result)
-            } catch (err) {
-                console.log(err)
-            }
-        }
-        fetchFoodsList()
-    }, [])
+        fetchFarmFoodTypes()
+    }, [fetchFarmFoodTypes])
 
     return (
         <Select
@@ -31,7 +23,7 @@ const FoodSelect = ({ onChange }) => {
             className={selectBox}
             loading={isLoading}
         >
-            {listOfFoods.map((food, i) => (
+            {farmFoodTypes.map((food, i) => (
                 <Option key={i} value={food}>
                     {food}
                 </Option>
