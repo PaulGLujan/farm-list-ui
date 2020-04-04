09ef4cd7f36780a1ae7c6002c9c539f195cf6957
Paul <paulglujan@gmail.com>
2020-04-04 16:47:36 -0700

    Converted map component to functional component

--- food-supplier-mkt-app/clientside-reactjs/src/components/mapbox/Mapbox.js
+++ food-supplier-mkt-app/clientside-reactjs/src/components/mapbox/Mapbox.js
@@ -1,77 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pins from './Pins';
-import { Component } from 'react';
 import ReactMapGL, { Popup } from 'react-map-gl';
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_API_TOKEN;
 
-class Map extends Component {
-    state = {
-        viewport: {
-            width: '100%',
-            height: '100%',
-            latitude: 37.7577,
-            longitude: -122.4376,
-            zoom: 8
-        },
-        hoverInfo: null
-    };
+const Map = () => {
+    const [viewport, setViewport] = useState({
+        width: '100%',
+        height: '100%',
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8
+    });
 
-    renderPopup() {
-        const { hoverInfo } = this.state;
-        if (hoverInfo) {
-            return (
-                <Popup
-                    longitude={hoverInfo.longitude}
-                    latitude={hoverInfo.latitude}
-                    closeButton={false}
-                >
-                    <div className="county-info">This is a popup</div>
-                </Popup>
-            );
-        }
-        return null;
-    }
+    const [hoverInfo, setHoverInfo] = useState(null);
 
-    onMarkerHover(togglePopup) {
+    const onMarkerHover = togglePopup => {
         if (togglePopup) {
             const hoverInfo = {
                 longitude: -122.420679,
                 latitude: 37.772537
             };
-            this.setState({ hoverInfo });
+            setHoverInfo(hoverInfo);
         } else {
-            this.setState({ hoverInfo: null });
+            setHoverInfo(null);
         }
-    }
+    };
 
-    render() {
-        return (
-            <div
-                style={{
-                    position    : 'absolute',
-                    width       : '100%',
-                    height      : '100%',
-                    overflow    : 'hidden',
-                    visibility  : 'inherit'
-                }}
+    return (
+        <div
+            style={{
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                overflow: 'hidden',
+                visibility: 'inherit'
+            }}
+        >
+            <ReactMapGL
+                {...viewport}
+                onViewportChange={setViewport}
+                mapboxApiAccessToken={MAPBOX_TOKEN}
+                mapStyle="mapbox://styles/mapbox/streets-v11"
             >
-                <ReactMapGL
-                    {...this.state.viewport}
-                    onViewportChange={viewport => this.setState({ viewport })}
-                    mapboxApiAccessToken={MAPBOX_TOKEN}
-                    mapStyle="mapbox://styles/mapbox/streets-v11"
-                >
-                    <Pins
-                        onHover={togglePopup => {
-                            this.onMarkerHover(togglePopup);
-                        }}
-                    />
-                    {this.renderPopup()}
-                </ReactMapGL>
-            </div>
+                <Pins
+                    onHover={togglePopup => {
+                        onMarkerHover(togglePopup);
+                    }}
+                />
+                {renderPopup({ hoverInfo })}
+            </ReactMapGL>
+        </div>
+    );
+};
+
+const renderPopup = ({ hoverInfo }) => {
+    if (hoverInfo) {
+        return (
+            <Popup
+                longitude={hoverInfo.longitude}
+                latitude={hoverInfo.latitude}
+                closeButton={false}
+            >
+                <div className="county-info">This is a popup</div>
+            </Popup>
         );
     }
-}
+    return null;
+};
 
 export default Map;
