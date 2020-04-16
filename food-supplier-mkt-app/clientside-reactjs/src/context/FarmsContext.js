521ddc832f4064beee100377dfa50189e41f65b3
lmjcbs <hello@liamjacobs.dev>
2020-04-16 20:45:32 +0100

    refactor to use FarmsContext

--- food-supplier-mkt-app/clientside-reactjs/src/context/FarmsContext.js
+++ food-supplier-mkt-app/clientside-reactjs/src/context/FarmsContext.js
@@ -1,84 +1,58 @@
 import React, { createContext, useState } from 'react';
 import { getAllAirtableData } from '../utilities/requestOperations';
 
-const initialFarms = {
+const initialFarmsData = {
     farms: [],
-    farmFoodTypes: []
+    farmFoodTypes: [],
+    filteredFarms: []
 }
 
-const FarmsContext = createContext(initialFarms)
+const FarmsContext = createContext(initialFarmsData)
 const { Provider } = FarmsContext
 
 const FarmsContextController = ({ children }) => {
-    const [farms, setFarms] = useState(initialFarms.farms)
-    const [farmFoodTypes, setFarmFoodTypes] = useState(initialFarms.farmFoodTypes)
+    const [farmsData, setFarmsData] = useState(initialFarmsData)
 
-    const fetchAllFarmData = async () => {
+    const fetchAllFarmData = () => {
+        return new Promise(async (resolve, reject) => {
             try {
                 // getAllAirtableData() returns an object containing two arrays
                 // for complete farm data and unique food types.
                 const response = await getAllAirtableData();
+                console.log(response)
                 const { farms, uniqueFarmFoodTypes } = response
-                await setFarms(farms)
-                await setFarmFoodTypes(uniqueFarmFoodTypes)
-                // resolve(farms);
+
+                const newFarmData = {
+                    ...farmsData,
+                    farms,
+                    farmFoodTypes: uniqueFarmFoodTypes,
+                }
+
+                console.log(newFarmData)
+                await setFarmsData(newFarmData)
+
+                resolve(farms);
             } catch (err) {
                 console.log(err)
-                // reject(err);
+                reject(err);
             }
-        // });
+        });
+    }
+
+    const filterFarms = filteredFarms => {
+        const newFarmData = {
+            ...farmsData,
+            filteredFarms
+        }
+
+        setFarmsData(newFarmData)
     }
 
     return (
-        <Provider value={{ farms, farmFoodTypes, setFarms, setFarmFoodTypes, fetchAllFarmData }}>
+        <Provider value={{ farmsData, fetchAllFarmData, filterFarms }}>
             {children}
         </Provider>
     );
 };
 
 export { FarmsContext, FarmsContextController };
-
-// import React, { useState } from 'react';
-// import FarmContext from './farm-context';
-// import { getAllAirtableData } from '../utilities/requestOperations';
-
-// const GlobalState = props => {
-//   const [farms, setFarms] = useState([])
-//   const [farmFoodTypes, setFarmFoodTypes] = useState([])
-
-//   const fetchAllFarmData = () => {
-//     return new Promise(async (resolve, reject) => {
-//     try {
-//         // getAllAirtableData() returns an object containing two arrays
-//         // for complete farm data and unique food types.
-//         const response = await getAllAirtableData();
-//         const { farms, uniqueFarmFoodTypes } = response
-//         await setFarms(farms)
-//         await setFarmFoodTypes(uniqueFarmFoodTypes)
-//         resolve(farms);
-//     } catch (err) {
-//         console.log(err)
-//         reject(err);
-//     }
-//   });
-//   }
-
-//   // Values are global through useContext
-//   // Example usage:
-//   //
-//   // ** import React, { useContext } from 'react' **
-//   // ** import FarmContext from './context/farm-context.js **
-//   //
-//   // const { farms } = useContext(FarmContext)
-//   return (
-//     <FarmContext.Provider value={{
-//       farms,
-//       farmFoodTypes,
-//       fetchAllFarmData,
-//     }}>
-//       {props.children}
-//     </FarmContext.Provider>
-//   )
-// }
-
-// export default GlobalState;
