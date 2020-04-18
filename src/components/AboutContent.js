2be9c1aad183956ab1488e965c8fe8eb08409a5e
codybreene <codybreene@gmail.com>
2020-04-17 17:14:48 -0700

    add logo and about link and about component

---
+++ src/components/AboutContent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutContent = () => {
+  return(
+    <p id="about">
+      The farm-to-table supply chain is deeply affected by the Covid-19 pandemic. Restaurants are hurting and so are many of the country's finest farmers that supply them. 
+
+      Farmlist is an online resource connecting the consumers to ranchers, fishermen, vegetable, dairy, and poultry farmers in the U.S. These farmers are now selling direct to consumer, whether it's through a CSA or a webstore, in some cases for the first time.
+
+      Our goal is to highlight these amazing purveyors and connect them with passionate people who want access to their products.
+
+      If you're a farmer or supplier and would like to be added to our list, please fill out this form [add link] and we'll add you to Farmlist. 
+    </p>
+  )
+}
+
+export default AboutContent
